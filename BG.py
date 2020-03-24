f37818c9996bd150f73242c239cb75bd2310d6c1
mrvx666 <43062346+mrvx666@users.noreply.github.com>
2020-03-24 14:55:01 +0800

    fix

--- BG.py
+++ BG.py
@@ -8,6 +8,7 @@ BG_header_row, BG_header_col = get_BG_header()
 
 def bg_read(sheet):
     outputarr = []
+    zk_name = {}
     for row in range(sheet.nrows)[BG_header_row:]:  # [1:]是用来跳过第一行的,这行通常是表头
         temp = BG_dict.copy()  # 拷贝原始字典
         for col in range(sheet.ncols)[BG_header_col:]:
@@ -18,6 +19,7 @@ def bg_read(sheet):
             else:
                 pass
             temp.update({'参与否': 1})
+            temp.update({'特征值': 0})
             outputdict = {"钻孔编号": zk_name,
                           "标贯数据": temp}
 
